af8bce96b08dc84c80003a185bf6b3d92ed017ab
Unknown <jokke.mero@gmail.com>
2019-05-30 15:57:10 +0300

    Started working on a new version of HalkoMethods
    Started creating a HalkoClass-script, which is attached to the classes where the HalkoMethod-methods are.

--- Winsock_test/Assets/Scripts/HalkoNetworking/HalkoMethod.cs
+++ Winsock_test/Assets/Scripts/HalkoNetworking/HalkoMethod.cs
@@ -1,16 +1,68 @@
-﻿using System;
+﻿using UnityEngine;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
-using HalkoNetworking;
 
 namespace HalkoNetworking.RemoteMethod
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class HalkoMethod : Attribute
     {
+    }
+    public class HalkoClass : MonoBehaviour
+    {
         //Private fields:
         private List<KeyValuePair<string, MethodInfo>> halkoMethods;
+        private HalkoNetwork halkoNetwork;
         private HalkoAttributeHandler ah;
+        private Formatter formatter;
+
+        private void Awake()
+        {
+            try
+            {
+                halkoNetwork = FindObjectOfType<HalkoNetwork>();
+            }
+            catch(Exception e)
+            {
+                Debug.Log("No HalkoNetwork-object found in the scene. " + e.Message);
+            }
+
+            formatter = new Formatter();
+            GetHalkoMethods();
+        }
+        //Public methods:
+
+        /// <summary>
+        /// Used to call a method locally and remotely.
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="parameters"></param>
+        public void InvokeMethod(string methodName, object[] parameters)
+        {
+            bool methodFound = false;
+            //If the methodName matches the key of a key-value -pair
+            //in the dictionary.
+            for (int i = halkoMethods.Count - 1; i >= 0; --i)
+            {
+                if (halkoMethods[i].Key == methodName)
+                {
+                    print(i);
+                    methodFound = true;
+                    //Call the method locally.
+                    halkoMethods[i].Value.Invoke(this, parameters);
+
+                    byte[] methodData = formatter.SerializeMethod2((byte)'m', i, parameters);
+                    halkoNetwork.stream.Write(methodData, 0, methodData.Length);
+                }
+            }
+            //If method wasn't found, throw an error.
+            if (!methodFound)
+            {
+                throw new Exception("No method with with this name was found.");
+            }
+        }
+
         //Private methods:
         private void GetHalkoMethods()
         {
